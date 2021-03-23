a92045c3dca4d2c00d63bc61e92f0e6348e11ceb
tokuyasu <fujiiakito3633@gmail.com>
2021-03-23 21:58:37 +0900

    first

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,12 @@
+<div>
+  <footer>
+    <img class="logo" src="img/IMG_0674.gif">
+    <div class="footer-nav">
+      <p >メニュー</p>
+      <p >店舗情報</p>
+      <p >お知らせ</p>
+      <p >お問い合わせ</p>
+    </div>
+  </footer>
+  <p id="copyright">Copyright. aaaaaaaaaaa. All Rights Reserve</p>
+</div>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,9 @@
+<header>
+  <img class="logo" src="img/IMG_0674.gif">
+  <div class="footer-nav">
+    <p >メニュー</p>
+    <p >店舗情報</p>
+    <p >お知らせ</p>
+    <p >お問い合わせ</p>
+  </div>
+</header>
